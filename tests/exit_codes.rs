5d396b251b4cda4a83d14543898d278482e92d37
Kengo TODA <toda_k@henry.jp>
2026-02-21 20:14:57 +0800

    test: make integration test running without debug build
    Signed-off-by: Kengo TODA <toda_k@henry.jp>

--- tests/exit_codes.rs
+++ tests/exit_codes.rs
@@ -2,19 +2,7 @@ use std::process::Command;
 
 #[test]
 fn inspequte_exits_non_zero_on_error() {
-    let inspequte = std::env::var("CARGO_BIN_EXE_inspequte")
-        .or_else(|_| std::env::var("CARGO_BIN_EXE_inspequte"))
-        .unwrap_or_else(|_| {
-            let mut path = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-            path.push("target");
-            path.push("debug");
-            path.push("inspequte");
-            if cfg!(windows) {
-                path.set_extension("exe");
-            }
-            path.to_string_lossy().to_string()
-        });
-    let output = Command::new(inspequte)
+    let output = Command::new(env!("CARGO_BIN_EXE_inspequte"))
         .arg("--input")
         .arg("missing.class")
         .output()
